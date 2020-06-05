b5b5e3bc2d2d194a02165460705d7152fc059d97
Aienobe Victor <viktornobe@gmail.com>
2020-06-05 14:40:45 +0100

    Revert "new.."

--- scripts/kosis_script.py
+++ scripts/kosis_script.py
@@ -2,6 +2,4 @@ email='anowi_5@yahoo.com'
 name= 'ANOWI KOSI'
 ID= 'HNG-02205'
 language='PYTHON'
-
-#output
-print('Hello World, this is',name,'with HNGi7 ID',ID,'using',language,'for stage 2 task','email',email,)
+print('Hello World this is',name,'with HNGi7 ID',ID,'using',language,'for stage 2 task','email',email,)
